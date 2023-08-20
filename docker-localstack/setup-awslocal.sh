ca12d0aaa69161e7b8c5afcb23b5c2d26c5ab227
RamuniN <navya.ramuni@communities.gov.uk>
2023-08-20 22:15:16 +0100

    added localstack service for SQS

---
+++ docker-localstack/setup-awslocal.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Create the bucket using awslocal
+if awslocal s3 ls | grep -q $AWS_BUCKET_NAME; then
+  echo "Bucket already exists!"
+else
+  # awslocal s3 mb s3://$AWS_BUCKET_NAME --region $AWS_REGION
+  awslocal s3api \
+  create-bucket --bucket $AWS_BUCKET_NAME \
+  --create-bucket-configuration LocationConstraint=$AWS_REGION \
+  --region $AWS_REGION
+
+  echo "Created Bucket $AWS_BUCKET_NAME!"
+
+fi
+
+
+# Create the SQS Queue using awslocal
+if awslocal sqs list-queues | grep -q $AWS_SQS_QUEUE_NAME; then
+  echo "$AWS_SQS_QUEUE_NAME already exists!"
+else
+  awslocal sqs create-queue --queue-name $AWS_SQS_QUEUE_NAME --region $AWS_REGION
+  echo "Created SQS Queue: $AWS_SQS_QUEUE_NAME!"
+
+fi
