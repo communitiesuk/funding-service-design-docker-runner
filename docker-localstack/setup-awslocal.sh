0321b1c48c843749cdefe96138efeffbf03d0e74
tferns <thomas.ferns@version1.com>
2023-08-21 11:51:26 +0100

    FS-3283: Add cors rule to localstack s3

--- docker-localstack/setup-awslocal.sh
+++ docker-localstack/setup-awslocal.sh
@@ -9,9 +9,19 @@ else
   create-bucket --bucket $AWS_BUCKET_NAME \
   --create-bucket-configuration LocationConstraint=$AWS_REGION \
   --region $AWS_REGION
-
   echo "Created Bucket $AWS_BUCKET_NAME!"
-
+  awslocal s3api \
+  put-bucket-cors --bucket $AWS_BUCKET_NAME \
+  --cors-configuration '{
+    "CORSRules": [
+      {
+        "AllowedOrigins": ["*"],
+        "AllowedMethods": ["GET", "PUT", "POST", "DELETE", "HEAD"],
+        "AllowedHeaders": ["*"]
+      }
+    ]
+  }'
+  echo "Created CORS rule!"
 fi
 
 
@@ -21,5 +31,4 @@ if awslocal sqs list-queues | grep -q $AWS_SQS_QUEUE_NAME; then
 else
   awslocal sqs create-queue --queue-name $AWS_SQS_QUEUE_NAME --region $AWS_REGION
   echo "Created SQS Queue: $AWS_SQS_QUEUE_NAME!"
-
 fi
