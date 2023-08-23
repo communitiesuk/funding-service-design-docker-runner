d645d77117b4876a9b0007e4faf715e99c7a0dfa
RamuniN <navya.ramuni@communities.gov.uk>
2023-08-23 15:42:30 +0100

    add dlq setup cmds

--- docker-localstack/setup-awslocal.sh
+++ docker-localstack/setup-awslocal.sh
@@ -25,10 +25,19 @@ else
 fi
 
 
-# Create the SQS Queue using awslocal
+# Create the SQS Queue & DLQ using awslocal
+if awslocal sqs list-queues | grep -q $AWS_DLQ_QUEUE_NAME; then
+  echo "$AWS_DLQ_QUEUE_NAME already exists!"
+else
+  awslocal sqs create-queue --queue-name $AWS_DLQ_QUEUE_NAME --region $AWS_REGION
+  echo "Created DLQ: $AWS_DLQ_QUEUE_NAME!"
+fi
+
 if awslocal sqs list-queues | grep -q $AWS_SQS_QUEUE_NAME; then
   echo "$AWS_SQS_QUEUE_NAME already exists!"
 else
-  awslocal sqs create-queue --queue-name $AWS_SQS_QUEUE_NAME --region $AWS_REGION
+  awslocal sqs create-queue --queue-name $AWS_SQS_QUEUE_NAME \
+   --region $AWS_REGION \
+  --attributes "{\"RedrivePolicy\":\"{\\\"deadLetterTargetArn\\\":\\\"arn:aws:sqs:$AWS_REGION:000000000000:$AWS_DLQ_QUEUE_NAME\\\",\\\"maxReceiveCount\\\":\\\"$AWS_DLQ_MAX_RECEIVE_COUNT\\\"}\"}"
   echo "Created SQS Queue: $AWS_SQS_QUEUE_NAME!"
 fi
