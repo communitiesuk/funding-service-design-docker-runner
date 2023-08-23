e1656c234303358574840d77e088afc3f98ffc7f
RamuniN <navya.ramuni@communities.gov.uk>
2023-08-23 17:48:08 +0100

    fixed dlq setup in bash script

--- docker-localstack/setup-awslocal.sh
+++ docker-localstack/setup-awslocal.sh
@@ -29,15 +29,18 @@ fi
 if awslocal sqs list-queues | grep -q $AWS_DLQ_QUEUE_NAME; then
   echo "$AWS_DLQ_QUEUE_NAME already exists!"
 else
-  awslocal sqs create-queue --queue-name $AWS_DLQ_QUEUE_NAME --region $AWS_REGION
-  echo "Created DLQ: $AWS_DLQ_QUEUE_NAME!"
+  DLQ_ARN=$(awslocal sqs create-queue --queue-name $AWS_DLQ_QUEUE_NAME  --region $AWS_REGION --output json | grep -o '"QueueUrl": "[^"]*' | awk -F'"' '{print $4}')
+  echo "Created DLQ: $DLQ_ARN!"
 fi
 
 if awslocal sqs list-queues | grep -q $AWS_SQS_QUEUE_NAME; then
   echo "$AWS_SQS_QUEUE_NAME already exists!"
 else
-  awslocal sqs create-queue --queue-name $AWS_SQS_QUEUE_NAME \
-   --region $AWS_REGION \
-  --attributes "{\"RedrivePolicy\":\"{\\\"deadLetterTargetArn\\\":\\\"arn:aws:sqs:$AWS_REGION:000000000000:$AWS_DLQ_QUEUE_NAME\\\",\\\"maxReceiveCount\\\":\\\"$AWS_DLQ_MAX_RECEIVE_COUNT\\\"}\"}"
+  SQS_QUERY_URL=$(awslocal sqs create-queue --queue-name $AWS_SQS_QUEUE_NAME  --region $AWS_REGION --output json | grep -o '"QueueUrl": "[^"]*' | awk -F'"' '{print $4}')
+  DLQ_QUEUE_ARN="arn:aws:sqs:$AWS_REGION:000000000000:$AWS_DLQ_QUEUE_NAME"
+
+  awslocal sqs set-queue-attributes --queue-url $SQS_QUERY_URL \
+  --attributes "{\"RedrivePolicy\":\"{\\\"deadLetterTargetArn\\\":\\\"$DLQ_QUEUE_ARN\\\",\\\"maxReceiveCount\\\":\\\"$AWS_DLQ_MAX_RECIEVE_COUNT\\\"}\"}"
+
   echo "Created SQS Queue: $AWS_SQS_QUEUE_NAME!"
 fi
