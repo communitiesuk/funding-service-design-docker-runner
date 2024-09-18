553af3c46091773aae611d91223b50cd66c5bab3
Nuwan Samarassinghe <nuwansamarasinghe100@gmail.com>
2024-09-18 11:36:23 +0100

    db url fix for each env (#43)
    * db url fix for each env
    
    * db url fix for each env
    
    * db url fix for each env

--- config/envs/default.py
+++ config/envs/default.py
@@ -1,5 +1,6 @@
 import logging
 from os import getenv
+from os import environ
 
 from fsd_utils import configclass
 
@@ -15,3 +16,4 @@ class DefaultConfig(object):
     FAB_SAVE_PER_PAGE = getenv("FAB_SAVE_PER_PAGE", "dev/save")
     FORM_RUNNER_URL = getenv("FORM_RUNNER_INTERNAL_HOST", "http://form-runner:3009")
     FORM_RUNNER_URL_REDIRECT = getenv("FORM_RUNNER_EXTERNAL_HOST", "http://localhost:3009")
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
--- config/envs/dev.py
+++ config/envs/dev.py
@@ -12,5 +12,3 @@ from config.envs.default import DefaultConfig
 class DevConfig(DefaultConfig):
     FSD_LOGGING_LEVEL = logging.INFO
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL").replace("postgres://", "postgresql://")
