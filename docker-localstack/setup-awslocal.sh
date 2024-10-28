61a47ac199448cd02a66ba67e5d7384ef941e1a1
Samuel Williams <samuelhwilliams@gmail.com>
2024-10-28 15:52:00 +0000

    Refactor localstack queue creation

--- docker-localstack/setup-awslocal.sh
+++ docker-localstack/setup-awslocal.sh
@@ -29,6 +29,35 @@ function create_aws_bucket {
   fi
 }
 
+function create_sqs_queue {
+  QUEUE_NAME="$1"
+  DLQ_QUEUE_NAME="$2"
+  MAX_RECEIVE_COUNT="$3"
+
+  if awslocal sqs list-queues | grep -q $DLQ_QUEUE_NAME; then
+    echo "$DLQ_QUEUE_NAME already exists!"
+  else
+    DLQ_QUEUE_URL=$(awslocal sqs create-queue --queue-name $DLQ_QUEUE_NAME --region $AWS_REGION --attributes FifoQueue=true --output json | grep -o '"QueueUrl": "[^"]*' | awk -F'"' '{print $4}')
+    echo "Created DLQ: $DLQ_QUEUE_URL!"
+  fi
+
+  if awslocal sqs list-queues | grep -q $QUEUE_NAME; then
+    echo "$QUEUE_NAME already exists!"
+  else
+    SQS_QUEUE_URL=$(awslocal sqs create-queue --queue-name $QUEUE_NAME --region $AWS_REGION --attributes FifoQueue=true --output json | grep -o '"QueueUrl": "[^"]*' | awk -F'"' '{print $4}')
+    DLQ_QUEUE_ARN="arn:aws:sqs:$AWS_REGION:000000000000:$DLQ_QUEUE_NAME"
+
+    # configure DLQ for current queue
+    awslocal sqs set-queue-attributes --queue-url $SQS_QUEUE_URL \
+    --attributes "{\"RedrivePolicy\":\"{\\\"deadLetterTargetArn\\\":\\\"$DLQ_QUEUE_ARN\\\",\\\"maxReceiveCount\\\":\\\"$MAX_RECEIVE_COUNT\\\"}\"}"
+
+    # set DeduplicationScope='queue', ensures strict message uniqueness across the entire queue
+    awslocal sqs set-queue-attributes --queue-url $SQS_QUEUE_URL --attributes DeduplicationScope=queue
+
+    echo "Created SQS Queue: $SQS_QUEUE_URL!"
+  fi
+}
+
 # <- Pre-award environment ->
 
 # Create buckets using awslocal
@@ -36,53 +65,8 @@ for bucket in "$AWS_BUCKET_NAME" "$AWS_MSG_BUCKET_NAME"; do
   create_aws_bucket "$bucket"
 done
 
-# Create the Fifo SQS Queue & DLQ using awslocal
-if awslocal sqs list-queues | grep -q $AWS_DLQ_IMPORT_APP_QUEUE_NAME; then
-  echo "$AWS_DLQ_IMPORT_APP_QUEUE_NAME already exists!"
-else
-  DLQ_QUEUE_URL=$(awslocal sqs create-queue --queue-name $AWS_DLQ_IMPORT_APP_QUEUE_NAME --region $AWS_REGION --attributes FifoQueue=true --output json | grep -o '"QueueUrl": "[^"]*' | awk -F'"' '{print $4}')
-  echo "Created DLQ: $DLQ_QUEUE_URL!"
-fi
-
-if awslocal sqs list-queues | grep -q $AWS_SQS_IMPORT_APP_QUEUE_NAME; then
-  echo "$AWS_SQS_IMPORT_APP_QUEUE_NAME already exists!"
-else
-  SQS_QUEUE_URL=$(awslocal sqs create-queue --queue-name $AWS_SQS_IMPORT_APP_QUEUE_NAME --region $AWS_REGION --attributes FifoQueue=true --output json | grep -o '"QueueUrl": "[^"]*' | awk -F'"' '{print $4}')
-  DLQ_QUEUE_ARN="arn:aws:sqs:$AWS_REGION:000000000000:$AWS_DLQ_IMPORT_APP_QUEUE_NAME"
-
-  # configure DLQ for current queue
-  awslocal sqs set-queue-attributes --queue-url $SQS_QUEUE_URL \
-  --attributes "{\"RedrivePolicy\":\"{\\\"deadLetterTargetArn\\\":\\\"$DLQ_QUEUE_ARN\\\",\\\"maxReceiveCount\\\":\\\"$AWS_DLQ_MAX_RECIEVE_COUNT\\\"}\"}"
-
-  # set DeduplicationScope='queue', ensures strict message uniqueness across the entire queue
-  awslocal sqs set-queue-attributes --queue-url $SQS_QUEUE_URL --attributes DeduplicationScope=queue
-
-  echo "Created SQS Queue: $SQS_QUEUE_URL!"
-fi
-
-# Create the Fifo SQS Queue & DLQ using awslocal for notification service
-if awslocal sqs list-queues | grep -q $AWS_DLQ_NOTIFICATION_APP_QUEUE_NAME; then
-  echo "$AWS_DLQ_NOTIFICATION_APP_QUEUE_NAME already exists!"
-else
-  DLQ_NOTIFICATION_QUEUE_URL=$(awslocal sqs create-queue --queue-name $AWS_DLQ_NOTIFICATION_APP_QUEUE_NAME --region $AWS_REGION --attributes FifoQueue=true --output json | grep -o '"QueueUrl": "[^"]*' | awk -F'"' '{print $4}')
-  echo "Created DLQ: $DLQ_NOTIFICATION_QUEUE_URL!"
-fi
-
-if awslocal sqs list-queues | grep -q $AWS_SQS_NOTIFICATION_APP_QUEUE_NAME; then
-  echo "$AWS_SQS_NOTIFICATION_APP_QUEUE_NAME already exists!"
-else
-  SQS_NOTIFICATION_QUEUE_URL=$(awslocal sqs create-queue --queue-name $AWS_SQS_NOTIFICATION_APP_QUEUE_NAME --region $AWS_REGION --attributes FifoQueue=true --output json | grep -o '"QueueUrl": "[^"]*' | awk -F'"' '{print $4}')
-  DLQ_NOTIFICATION_QUEUE_ARN="arn:aws:sqs:$AWS_REGION:000000000000:$AWS_DLQ_NOTIFICATION_APP_QUEUE_NAME"
-
-  # configure DLQ for current queue
-  awslocal sqs set-queue-attributes --queue-url $SQS_NOTIFICATION_QUEUE_URL \
-  --attributes "{\"RedrivePolicy\":\"{\\\"deadLetterTargetArn\\\":\\\"$DLQ_NOTIFICATION_QUEUE_ARN\\\",\\\"maxReceiveCount\\\":\\\"$AWS_DLQ_NOTIFICATION_MAX_RECIEVE_COUNT\\\"}\"}"
-
-  # set DeduplicationScope='queue', ensures strict message uniqueness across the entire queue
-  awslocal sqs set-queue-attributes --queue-url $SQS_NOTIFICATION_QUEUE_URL --attributes DeduplicationScope=queue
-
-  echo "Created SQS Queue: $SQS_NOTIFICATION_QUEUE_URL!"
-fi
+create_sqs_queue "$AWS_SQS_IMPORT_APP_QUEUE_NAME" "$AWS_DLQ_IMPORT_APP_QUEUE_NAME" "$AWS_DLQ_MAX_RECEIVE_COUNT"
+create_sqs_queue "$AWS_SQS_NOTIFICATION_APP_QUEUE_NAME" "$AWS_DLQ_NOTIFICATION_APP_QUEUE_NAME"
 # <- Pre-award environment ->
 
 
