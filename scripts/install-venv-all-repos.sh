d54f2a07ff562385308285733656462edd329c11
Samuel Williams <samuelhwilliams@gmail.com>
2024-12-09 14:14:16 +0000

    Migrate frontend app to combined pre-award-frontend
    We've lifted and shifted the code for `funding-service-design-frontend`
    into `funding-service-pre-award-frontend`, as part of our efforts to
    consolidate from many repos/apps to closer-to-one.
    
    This change updates the docker runner to use the new pre-award-frontend
    repo.

--- scripts/install-venv-all-repos.sh
+++ scripts/install-venv-all-repos.sh
@@ -5,7 +5,7 @@ build_static_files=false
 
 repo_root=$(dirname $(dirname $(realpath $0)))
 workspace_dir="${repo_root}/apps"
-declare -a repos=("funding-service-design-fund-application-builder" "funding-service-pre-award-stores" "funding-service-design-authenticator" "funding-service-design-assessment" "funding-service-design-account-store" "funding-service-design-frontend" "funding-service-design-notification" "funding-service-design-post-award-data-store")
+declare -a repos=("funding-service-design-fund-application-builder" "funding-service-pre-award-stores" "funding-service-design-authenticator" "funding-service-design-assessment" "funding-service-design-account-store" "funding-service-pre-award-frontend" "funding-service-design-notification" "funding-service-design-post-award-data-store")
 
 while getopts 'vps' OPTION; do
     case "$OPTION" in
